76c8eb9b81f1c2e93d87c8a6d88ea5b8f77041e7
pyfection <matthias.schreiber@zoho.com>
2022-11-18 16:31:36 -0300

    Fix serialisation issue

--- models/_base.py
+++ models/_base.py
@@ -12,15 +12,17 @@ db = TinyDB(f"dbs/{os.getenv('NATIVO_LANG')}.json")
 @dataclass(kw_only=True)
 class Model(ABC):
     id: str = field(default_factory=lambda: str(uuid4()))
-    created_at: datetime = field(default_factory=datetime.utcnow)
+    created_at: str = field(
+        default_factory=lambda: datetime.utcnow().isoformat())
 
     class Meta:
         table_name: str = None
 
     @classmethod
     def get(cls, id) -> "Model":
-        word = db.table(cls.Meta.table_name).search(Query().id == id)[0]
-        return cls(**word)
+        data = db.table(cls.Meta.table_name).search(Query().id == id)[0]
+        model = cls(**data)
+        return model
 
     @classmethod
     def all(cls):
