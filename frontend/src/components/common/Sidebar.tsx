f6440a15357d3dc1aafecc53fe91b4daa0b71fcb
pyfection <mat@pyfection.com>
2025-11-09 14:18:58 -0300

    Update words sidebar icon

--- frontend/src/components/common/Sidebar.tsx
+++ frontend/src/components/common/Sidebar.tsx
@@ -13,8 +13,18 @@ const NAV_ITEMS: NavItem[] = [
     path: '/words',
     label: 'Words',
     icon: (
-      <svg width="18" height="18" viewBox="0 0 16 16" aria-hidden="true">
-        <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3z" />
+      <svg
+        width="18"
+        height="18"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="1.6"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        aria-hidden="true"
+      >
+        <path d="M4 7l3.2 10L12 7l4.8 10L20 7" />
       </svg>
     ),
   },
